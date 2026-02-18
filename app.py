ef462b522a07363fe7dafea093b948a8bfdd7495
chukiatkrab <chukiat.ka@kkumail.com>
2026-02-18 21:56:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -30,55 +30,13 @@ chol = st.number_input("Cholesterol", 100, 600)
 fbs = st.selectbox("Fasting Blood Sugar", [0, 1])
 restecg = st.selectbox("Rest ECG", [0, 2])
 thalach = st.number_input("Max Heart Rate", 60, 220)
-exang = st.selectbox("Exercise Induced Angina", [0, 1])
-oldpeak = st.number_input("Oldpeak", 0.0, 6.0)
-slope = st.selectbox("Slope", [0, 1, 2])
-ca = st.selectbox("CA", [0, 1, 2, 3])
-thal = st.selectbox("Thal", [0, 1, 2, 3])
 
 if st.button("Predict"):
 
-    input_data = np.array([[
-        age, sex, cp, trestbps, chol,
-        fbs, restecg, thalach,
-        exang, oldpeak, slope, ca, thal
-    ]])
-
+    input_data = np.array([[age, sex, cp, trestbps, chol, fbs, restecg, thalach]])
     prediction = model.predict(input_data)
 
     if prediction[0] == 1:
         st.error("Heart Disease Detected")
     else:
         st.success("No Heart Disease")
-
-# # =========================
-# # PART 2: CSV Upload
-# # =========================
-# st.header("Predict from CSV File")
-
-# uploaded_file = st.file_uploader("Upload CSV", type=["csv"])
-
-# if uploaded_file is not None:
-
-#     df = pd.read_csv(uploaded_file)
-
-#     st.write("CSV Preview:")
-#     st.dataframe(df)
-
-#     input_data = df[['age','sex','cp','trestbps','chol','fbs','restecg','thalach']].values
-
-#     prediction = model.predict(input_data)
-
-#     df['Prediction'] = prediction
-
-#     st.write("Prediction Result:")
-#     st.dataframe(df)
-
-#     csv = df.to_csv(index=False).encode('utf-8')
-
-#     st.download_button(
-#         label="Download Result CSV",
-#         data=csv,
-#         file_name="heart_prediction_result.csv",
-#         mime="text/csv"
-#     )
